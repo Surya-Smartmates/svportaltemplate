8a427dd20820ddb2ac1f3c8bbda50fceb69bab74
Surya-Smartmates <surya@smartmates.com.au>
2021-11-26 08:39:58 +0800

    latest update

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports ={
     domains: ["i.ibb.co"],
   },
   env:{
-    NEXTAUTH_URL:'http://localhost:3000'
+    NEXTAUTH_URL:'https://svportaltemplate.vercel.app'
   }
   // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
   //     // Note: we provide webpack above so you should not `require` it
