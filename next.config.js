6c6ac9afef0aa650d4e7eedfd4e2a2f4d82f1f5f
Tahir Mushtaq <tahirmushtaq5@gmail.com>
2021-12-07 21:42:08 +0500

    url update to local

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports ={
     domains: ["i.ibb.co"],
   },
   env:{
-    NEXTAUTH_URL:'https://svportaltemplate.vercel.app'
+    NEXTAUTH_URL:'http://localhost:3000'
   }
   ,
    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
