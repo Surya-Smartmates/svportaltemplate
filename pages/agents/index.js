3123eab59cfc4d67518a4ef83ee161530f3e0ddd
Surya-Smartmates <surya@smartmates.com.au>
2021-12-16 15:12:31 +0800

    Stress Test 16th December 2021

--- pages/agents/index.js
+++ pages/agents/index.js
@@ -1,21 +1,15 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import dynamic from "next/dynamic";
-// import AgentHome from "./components/AgentHome/AgentHome";
+
 import NavbarAgent from "../../components/Shared/Navbar/Navbar-Agent";
 import Sidebar from "../../components/Shared/Sidebar/Sidebar";
-// import "./style.css";
-// import "./responsive.css";
-// import happenbg from "../../assets/img/happen-bg.png";
-
 
 import applybtn from "../../assets/agents/img/just-blue.png"
 
-import social from "../../assets/agents/img/social-group.svg";
 import fb from "../../assets/img/facebook-fix-green.svg"
 import yt from "../../assets/img/youtube-green.svg"
 import ig from "../../assets/img/instagram-fix-green.svg"
 import lkin from "../../assets/img/linkedin-fix-green.svg"
-import feedback from "../../assets/agents/img/feedback.svg";
 
 import book from "../../assets/agents/img/resolve-page2.png";
 
@@ -28,11 +22,7 @@ import * as cookie from "cookie";
 import axios from "axios";
 import _ from "lodash";
 
-// import Chart from "../../components/Chart/Chart";
-const Chart = dynamic(
-  async () => await import("../../components/Chart/Chart"),
-  { ssr: false }
-);
+
 // console.log({ Chart });
 const AgentDashboard = ({
   portalUserResp,
@@ -42,6 +32,7 @@ const AgentDashboard = ({
 }) => {
   const router = useRouter();
   const state = useTrackedStore();
+
   const topbarLinks = [
     {
       href: "/profile",
@@ -51,33 +42,23 @@ const AgentDashboard = ({
   state.setAgentsResp(agentsResp);
   state.setPortalUserResp(portalUserResp);
   state.setStudentsResp(studentsResp);
-  const profile =
-    router.pathname.split("/")?.[1] ||
-    state?.portalUserResp?.User_Type?.toLowerCase() ||
-    "";
+  
 //Smartmates Code
 
 useEffect(()=>{
-  console.log(state?.agentsResp?.[0]?.id)
+  console.log("test check studentsupporter")
   console.log(state?.studentsResp)
+  let studentsBuffer = [...studentsList]
+  studentsBuffer = state?.studentsResp
+  setStudentsList(studentsBuffer)
 },[])
-  async function submitNewRecord (data){
-    const submittedData = {
-      name: data.target.name.value,
-      pref_contact: data.target.pref_contact.value,
-      course: data.target.course.value,
-      institution: data.target.institution.value,
-      program: data.target.program.value,
-      city: data.target.city.value,
-      agency: data.target.agency.value,
-      date: data.target.date.value
-    }
 
-    const addRecordResponse = await axios.post()
-    console.log(data.target.program.value)
-    console.log(submittedData)
-    data.preventDefault()
-  }
+useEffect(()=>{
+  console.log(studentsList)
+},[studentsList])
+
+const [studentsList, setStudentsList] = useState([])
+  
   const profileUserName = `${state?.agentsResp?.[0]?.Agency_Name || ""}`;
   const agentID = `${state?.agentsResp?.[0]?.id || ""}`
   return (
@@ -87,102 +68,14 @@ useEffect(()=>{
         <Sidebar />
         <div className="main-content">
           <div className="content-wrapper">
-            {/* hapaning-items */}
-            {/*<div className="happening-wrapper">
-              <div className="content-title">
-                <h4>What’s Happening</h4>
-              </div>
-              <div className="happenig-items d-grid">
-                
-                <div
-                  className="single-happen d-flex align-items-center justify-content-center"
-                  style={{
-                    backgroundImage: `url("${happenbg.src}")`,
-                  }}
-                >
-                  <div className="happen-count-text">
-                    {state?.agentsResp?.[0]?.No_of_Prospective_Students}
-                  </div>
-                  <div className="hapent-cotnet">
-                    <h4>LEADS</h4>
-                    <p>
-                      Follow Up for the chance <br />
-                      to earn 1500$
-                    </p>
-                  </div>
-                </div>
-                
-                {single-happening-items}
-                <div
-                  className="single-happen d-flex align-items-center justify-content-center"
-                  style={{
-                    backgroundImage: `url("${happenbg.src}")`,
-                  }}
-                >
-                  <div className="happen-count-text">
-                    {" "}
-                    {state?.agentsResp?.[0]?.No_of_Enrolled_Students}
-                  </div>
-                  <div className="hapent-cotnet">
-                    <h4>STUDENTS</h4>
-                    <p>
-                      Enrolled with <br />
-                      StudyVillage so far
-                    </p>
-                  </div>
-                </div>
-                {single-happening-items}
-                
-                <div
-                  className="single-happen d-flex align-items-center justify-content-center"
-                  style={{
-                    backgroundImage: `url("${happenbg.src}")`,
-                  }}
-                >
-                  <div className="happen-count-text">
-                    {" "}
-                    {state?.agentsResp?.[0]?.Applications_Submitted_This_Month}
-                  </div>
-                  <div className="hapent-cotnet">
-                    <h4>APPLICATION</h4>
-                    <p>
-                      Submitted this month <br />
-                      with StudyVillage
-                    </p>
-                  </div>
-                </div>
-                
-                <div
-                  className="single-happen d-flex align-items-center justify-content-center"
-                  style={{
-                    backgroundImage: `url("${happenbg.src}")`,
-                  }}
-                >
-                  <div className="happen-count-text">9</div>
-                  <div className="hapent-cotnet">
-                    <h4>NEW RESOURCE</h4>
-                    <p>
-                      Loaded to the portal. Visit the resource tab to increase
-                      your selling opportunities
-                    </p>
-                  </div>
-                </div>
-                 single-happening-items 
-              </div>
-            </div>*/}
-            {/* hapaning-items_End */}
-
-            {/* Apply Studies-items */}
+            
             <div className="content-title">
               <h4 className = "home-title">Welcome to your StudyVillage Portal</h4>
             </div>
             <div className = "row" style = {{ marginTop:"5%" }}>
               <div className = "col-md-4 apply-section">
                 <div style = {{ paddingTop: "20%" }} className="card-text">
-                  {/*<h4 className = "stu-app ">STUDENT APPLICATION</h4>
-                      <strong><i className = "stu-app-sub">
-                      Click here to launch application on behalf of your student
-                      </i></strong>*/}
+                  
                       <a 
                         href="https://forms.zohopublic.com/studyvillage/form/C1StudyVillageprogramapplicationforAgentPortal/formperma/avLZwfvjgtvfVp_2V4H7S9qlY2W2IGwI4OwlVVStMfs" 
                         target = "_blank" 
@@ -207,133 +100,11 @@ useEffect(()=>{
               </div>
               
             <div className="card-block-area">
-           
-              {/* single-card */}
-             {/**single-card-block white-box text-center */}
-              
-              {/* single-card */}
               <div style = {{ width: "200%" }}>
-              
               </div>
-             
             </div>
-            {/* Apply Studies-items_END */}
-
-            {/* commition-pipline */}
-            {/*<div className="commission-area white-box">
-              <div className="sec-title">
-                <h4>Commission Pipeline</h4>
-              </div>
-
-              <div className="pipline-cart-area">
-                <Chart />
-              </div>
-            </div>*/}
-            {/* commition-pipline */}
-            {/* register-area */}
-            <div className="register-lead-area d-grid">     
-              {/**//*<div className="rgister-lead-form white-box">
-                <h4>Register a Lead</h4>
-                <form  onSubmit = {submitNewRecord} >
-                  <div className="single-input-wrap">
-                    <label htmlFor="name">Name and Surname</label>
-                    <input id = "name" type="text" />
-                  </div>
-                  <div className="single-input-wrap">
-                    <label htmlFor>Preferred Contact</label>
-                    <input id = "pref_contact" type="text" />
-                  </div>
-                  <div className="single-input-wrap">
-                    <label htmlFor>Intended Course of Study</label>
-                    <input id = "course" type="text" />
-                  </div>
-                  <div className="single-input-wrap">
-                    <label htmlFor>Intended Institution</label>
-                    <input id = "institution" type="text" />
-                  </div>
-                  <div className="single-input-wrap">
-                    <label htmlFor>Location City/County</label>
-                    <input id = "city" type="text" />
-                  </div>
-                  <div className="single-input-wrap">
-                    <label htmlFor>Your Agency</label>
-                    <input id = "agency" type="text" />
-                  </div>
-                  <div className="single-input-wrap">
-                    <label htmlFor>StudyVillage Program (If Known)</label>
-                    <input id = "program" type="text" />
-                  </div>
-                  <div className="single-input-wrap">
-                    <label htmlFor>Date (DD/MM/YYYY)</label>
-                    <input id = "date" type="text" />
-                  </div>
-                  <div className="register-btn pt-3">
-                    <button type="submit" className="btn">
-                      Register
-                    </button>
-                  </div>
-                </form>
-              </div>*/}
-              {/*<div className="leads-wrapper white-box">
-                <h4>Leads</h4>
-                <div className="registerd-items">
-                  <div className="single-resgisterd d-flex align-items-start justify-content-between">
-                    <div className="menu-icon">
-                      <Image width={150} height={150} src={men} alt="icon" />
-                    </div>
-                    <div className="register-text">
-                      Casandra Pizarro, <a href>pizcas@hotmail.com</a>, Bachelor
-                      Degree in UK, Award Program on 24/09/2021
-                    </div>
-                    <button type="button">
-                      <Image
-                        width={150}
-                        height={150}
-                        src={trash}
-                        alt="trash icon"
-                      />
-                    </button>
-                  </div>
-                </div>*/}
-                {/* commissions */}
-                {/* <div className="cf-comission">
-                  <h6>Follow up for the chance to earn a commission of:</h6>
-                  <h1>$1500</h1>
-                  <span>*Commission is quoted in USD</span>
-                </div> */}
-                {/* commissions */}
-              {/*</div>*/}
-              {/* <div className="bulk-lead white-box">
-                <h4>Bulk Leads</h4>
-                <p>
-                  Do you have a large number of leads? You can upload an Excel
-                  File here for bulk leads. Please make sure any leads you
-                  provide will be tagged to your agency
-                </p>
-                <div className="upload-file-opt">
-                  <div className="up-file-btn">
-                    <input type="file" id file-upload />
-                    <label htmlFor="file-upload">Upload</label>
-                  </div>
-                  <div className="file-name-here">
-                    <div className="single-file-name d-flex align-items-center">
-                      <div className="up-file-icon">
-                        <Image width={50} height={50} src={xlfile} alt="icon" />
-                      </div>
-                      <div className="up-file-name">Leads_090921</div>
-                    </div>
-                    <div className="single-file-name d-flex align-items-center">
-                      <div className="up-file-icon">
-                        <Image width={50} height={50} src={xlfile} alt="icon" />
-                      </div>
-                      <div className="up-file-name">Leads_300821</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-             */}
+            <div className="register-lead-area d-grid">
             </div>
-            
              <h4 style = {{ fontSize: "0.9em" }}> Your StudyVillage Students</h4>
             <table className = "table table-striped table-bordered">
                         <thead>
@@ -350,7 +121,8 @@ useEffect(()=>{
                         </thead>
                         <tbody>
                         {
-                          state?.studentsResp?.map((student)=>{
+                          
+                           studentsList.map((student)=>{
                             return(
                               <tr className = "table-secondary">
                             <td>{student.Full_Name}</td>
@@ -363,7 +135,6 @@ useEffect(()=>{
                             <td>{student.Phone}</td>
                         </tr>
                             )
-                            
                           })
                         }
                         
